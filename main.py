d002a85a87e1e30e91658575a8d0b6df2ddb576b
Levatronische <95122626+Levatronische@users.noreply.github.com>
2022-01-22 22:28:06 +0300

    Input value T2

--- main.py
+++ main.py
@@ -6,11 +6,32 @@ print("Программа по визуализации фракталов\n"
       "Доступные фракталы:\n"
       "(Превым идёт название фрактала, вторым его имя в программе)\n"
       "***\n"
-      "Фрактал для Григория - grfr\n")
-name_fr_list = ["grfr"]
-name_fr = str(input("Введите имя фрактала:"))
-while name_fr not in name_fr_list:
-    print("Неверное имя фрактала")
-    name_fr = str(input("Введите имя фрактала:"))
+      "Кривая Дракона - dragon_fr\n")
+name_fr_list = ["dragon_fr"]
+#name_fr = str(input("Введите имя фрактала: "))
+#while name_fr not in name_fr_list:
+#    print("Ошибка, Неверное имя фрактала!")
+#    name_fr = str(input("Введите имя фрактала: "))
 
-iterations = int(float(input("Введите вещественное число, колличество итераций фрактала:")))
+#iterations = int(float(input("Введите целое число, колличество итераций фрактала: ")))
+#it_count = iterations
+speed = input("Введите целое число, скорость робота: ")
+tur = turtle.Turtle()
+tur.speed(int(speed))
+
+tur.getscreen().bgcolor("black")
+tur.color("cyan")
+tur.penup()
+tur.goto((0, 0))
+tur.pendown()
+
+
+dragon_fr_per = 1
+
+#def dragon_fr(iteration):
+#    while True:
+#        if it_count == 0:
+#           return
+
+
+turtle.done()
