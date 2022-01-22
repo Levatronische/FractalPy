75754bc5fdbdaa1dbb880a84e155d0eb6fafd99a
Levatronische <95122626+Levatronische@users.noreply.github.com>
2022-01-22 23:31:32 +0300

    Debug and exe v0.1.2

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ print("Программа по визуализации фракталов\n"
       "Елин Лев v0.1\n"
       "***\n"
       "Доступные фракталы:\n"
-      "(Превым идёт название фрактала, вторым его имя в программе)\n"
+      "(Первым идёт название фрактала, вторым его номер)\n"
       "***\n"
-      "Кривая Дракона - dragon_fr\n")
-name_fr_list = ["dragon_fr"]
-name_fr = str(input("Введите имя фрактала: "))
+      "Кривая Дракона - 1\n")
+name_fr_list = [1]
+name_fr = int(input("Введите номер фрактала: "))
 while name_fr not in name_fr_list:
-    print("Ошибка, Неверное имя фрактала!")
-    name_fr = str(input("Введите имя фрактала: "))
-
-iterations = int(float(input("Введите целое число, колличество итераций фрактала меньше 20: ")))
+    print("Ошибка, Неверный номер фрактала!")
+    name_fr = str(input("Введите номер фрактала: "))
+iterations = int(input("Введите целое число, количество итераций фрактала меньше 20: "))
+if iterations >= 20:
+    iterations = 19
 it_count = iterations
 speed = input("Введите целое число, скорость робота: ")
 tur = turtle.Turtle()
@@ -40,6 +41,8 @@ def dragon_fr(iteration):
         n = 5
     elif iteration > 8:
         n = 2
+    elif iteration > 15:
+        n = 0.1
     for i in dragon_fr_rot:
         if i == "R":
             tur.forward(n)
@@ -55,8 +58,7 @@ tur.penup()
 tur.goto((0, 0))
 tur.pendown()
 
-if name_fr == "dragon_fr":
+if name_fr == 1:
     dragon_fr(iterations)
-
 
 turtle.done()
